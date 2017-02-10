fce8fba8862cf53253344c75e3d9921d97e0733b
quaider <pto.kratos@hotmail.com>
2017-02-10 11:15:31 +0800

    12

---
+++ TongYan.Web/Areas/Demo/Controllers/SelectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TongYan.Web.Areas.Demo.Controllers
+{
+    public class SelectController : Controller
+    {
+        // GET: Demo/Select
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
